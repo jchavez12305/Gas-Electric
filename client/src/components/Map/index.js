3211ac1f403a1042910209b5dc438291e3b2eac7
Cedar Chavez <geggas00@gmail.com>
2022-12-14 00:01:21 -0800

    label maker

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -3,16 +3,15 @@ import { useState } from 'react';
 import './index.css';
 
 
-function MapContainer(props) {
+function MapContainer() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GM_API_KEY,
   })
 
   const [locationMap, setLocationMap] = useState({ lat: 34.064990, lng: -118.287300 });
-  const [zipcodeInput, setZipcodeInput] = useState("90057");
+  const [zipcodeInput, setZipcodeInput] = useState("");
   const [stationsFUEL, setstationsFUEL] = useState("");
   const [stationsEV, setstationsEV] = useState("");
-
 
 
   const callGeolocation = async () => {
@@ -27,24 +26,74 @@ function MapContainer(props) {
       }
 
       const { location } = await response.json();
+      // console.log(location);
+
+      const responseZip = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${process.env.REACT_APP_GM_API_KEY}`,
+        { method: 'POST' }
+      );
+
+      if (!responseZip.ok) {
+        throw new Error('something went wrong!');
+      }
+      const address = await responseZip.json();
+      let zip = address.results[0].address_components[7].short_name;
+      setZipcodeInput(...zipcodeInput, zip);
       setLocationMap({
         ...locationMap,
         lat: location.lat,
         lng: location.lng
       }
       );
-      newCenter(locationMap);
+      console.log(zipcodeInput,'geolocation');
+
+      search();
     } catch (err) {
       console.error(err.message);
     }
   };
 
-  async function newCenter(locationMap) {
-    let position;
-    locationMap?position=locationMap:position=this.getPosition().toJSON();
+  async function newCenter() {
+    let position = this.getPosition().toJSON();
+    geocode(position);  
+  }
+
+  async function geocode(coordinates){
     try {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.lat},${position.lng}&key=${process.env.REACT_APP_GM_API_KEY}`,
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates.lat},${coordinates.lng}&key=${process.env.REACT_APP_GM_API_KEY}`,
+        { method: 'POST' }
+      );
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+      setLocationMap({
+        ...locationMap,
+        lat: coordinates.lat,
+        lng: coordinates.lng
+      }
+      );
+      const address = await response.json();
+      let zip = address.results[0].address_components[7].short_name;
+      setZipcodeInput(...zipcodeInput, zip);
+      search();
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  } 
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    return name === setZipcodeInput(value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event ? event.preventDefault() : console.log('noEvent');
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcodeInput}&key=${process.env.REACT_APP_GM_API_KEY}`,
         { method: 'POST' }
       );
 
@@ -53,25 +102,29 @@ function MapContainer(props) {
       }
 
       const address = await response.json();
-      let zip = address.results[0].address_components[7].short_name;
-      setZipcodeInput(...zipcodeInput,zip);
-      handleFormSubmit();
+      let latlng = address.results[0].geometry.location;
+      setLocationMap({
+        ...locationMap,
+        lat: latlng.lat,
+        lng: latlng.lng
+      }
+      );
+      search();
 
     } catch (err) {
       console.error(err.message);
     }
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    return name === setZipcodeInput(value);
   };
 
-  const handleFormSubmit = async (event) => {
-    event?event.preventDefault():console.log('noEvent');
+  async function search() {
+    // let allMarkers = document.getElementsByClassName('marker');
+    // allMarkers.remove();
+    // labelIndex=0;
+    // fuelIndex=0;
     if (!zipcodeInput) {
       return false;
     }
+    console.log(zipcodeInput,'search');
     try {
       const responseEv = await fetch(
         `https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=${zipcodeInput}&${process.env.REACT_APP_EV_API_KEY}`
@@ -84,14 +137,15 @@ function MapContainer(props) {
         throw new Error('something went wrong!');
       }
       const stationsEv = await responseEv.json();
-      const stationsFuel = await responseFuel.json();
-      setZipcodeInput("");
-      setstationsFUEL(...stationsFUEL,stationsFuel);
-      setstationsEV(...stationsEV,stationsEv);
+      const stationsFuel = await responseFuel.json(); 
+      setstationsFUEL(...stationsFUEL, stationsFuel);
+      setstationsEV(...stationsEV, stationsEv);
+      // setZipcodeInput("");
     } catch (err) {
       console.error(err);
     }
-  };
+
+  }
 
   if (!isLoaded) return <div>Loading...</div>;
   return <div className='mapContainer'><Map
@@ -102,14 +156,25 @@ function MapContainer(props) {
     handleFormSubmit={handleFormSubmit}
     handleChange={handleChange}
     stationsEV={stationsEV}
-    stationsFUEL={stationsFUEL} /></div>;
+    stationsFUEL={stationsFUEL}
+    // labelIndex={labelIndex}
+    // fuelIndex={fuelIndex} 
+    /></div>;
 }
 
 function Map(props) {
   //this is so it doesnt reset when it rerenders
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let labelIndex = 0;
   let fuelIndex = 0;
+  
+  function labelMaker(index){
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let longerLabel = '';
+    for(let i = 0; i < index/labels.length; i++){
+      longerLabel+=labels[index%labels.length];
+    }
+    return longerLabel;
+  }
   return (
     <>
       <button onClick={props.geolocation}>Geolocation</button>
@@ -127,7 +192,6 @@ function Map(props) {
         mapContainerClassName="map">
         <MarkerF
           position={{ lat: props.coord.lat, lng: props.coord.lng }}
-          // label='^'
           title='user'
           id='user'
           draggable
@@ -135,7 +199,8 @@ function Map(props) {
         />
         {props.stationsEV.fuel_stations?.map((station) => {
           let position = { lat: station.latitude, lng: station.longitude };
-          let label = labels[labelIndex++ % labels.length];
+          labelIndex++;
+          let label = `E-${labelMaker(labelIndex)}`;
           let stationName = station.station_name;
           return (
             <MarkerF key={label} position={position} label={label} title={stationName} />
@@ -144,9 +209,9 @@ function Map(props) {
         {props.stationsFUEL.results?.map((station) => {
           let location = station.geometry.location;
           let position = { lat: location.lat, lng: location.lng };
-          let label = `G-${labels[fuelIndex++ % labels.length]}`;
+          fuelIndex++;
+          let label = `G-${labelMaker(fuelIndex)}`;
           let stationName = station.name;
-          let icon = station.icon
           return (
             <MarkerF key={label} position={position} label={label} title={stationName} />
           )
