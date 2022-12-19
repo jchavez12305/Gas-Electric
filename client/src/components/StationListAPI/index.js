ace6903105d6bdd1941833ff45fd625eade39d55
gyoon001 <108314452+gyoon001@users.noreply.github.com>
2022-12-19 14:29:17 -0800

    card for stationdetails

--- client/src/components/StationListAPI/index.js
+++ client/src/components/StationListAPI/index.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
 import './index.css'
+import Card from 'react-bootstrap/card'
+
+
+// const PlaceDetails = ({ place, selected, refProp }) => {
+//     if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//     const classes = useStyles();
+  
 function StationListAPI(props) {
 let labelIndex = 0;
 let fuelIndex = 0;
@@ -21,15 +28,23 @@ let fuelIndex = 0;
             let label = `${emoji.getUnicode("electric_plug")}${labelMaker(labelIndex)}`;
             labelIndex++;
             return(
+                <Card style={{width: '20rem' }}>
+                <Card.body>
+                    <Card.Title>
                 <div key={`RE-${station.id}`} className="stationName">
                     <h4>{label}</h4>
                     <h5>{station.station_name}</h5>
+                </div>
+                    </Card.Title>
+                <Card.Text>
                 <div class="addressDetails">
                     {station.street_address}, {station.city}, {station.state}, {station.zip}
                 </div>
-                </div>
+                </Card.Text>
+                </Card.body>
+                </Card>
             )})}
-            {props.stationsFUEL.results?.map((station) => {
+            {props.stationsFUEL.results?.map ((station) => {
             let label = `${emoji.getUnicode("fuelpump")}${labelMaker(fuelIndex)}`;
             fuelIndex++;
             return(
